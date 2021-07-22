b07b75231c0b5246275c8dc2465ffb84ccb9f89d
Vitaly Pokrova <vitalik_1254@mail.ru>
2021-07-22 11:36:03 +0300

    .gradle deleted from origin/vitaly

--- src/main/java/application/сontrollers/DefaultController.java
+++ src/main/java/application/сontrollers/DefaultController.java
@@ -1,8 +1,7 @@
-package application.Controllers;
+package application.сontrollers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class DefaultController {
