e9be1c5660d1a50da7ee789c711b494ef9b6b6ec
Vitaly Pokrova <vitalik_1254@mail.ru>
2021-07-27 15:08:39 +0300

    implemented POST api/account/register, created GlobalExceptionHandler

---
+++ src/main/java/application/exceptions/ErrorResponse.java
@@ -0,0 +1,15 @@
+package application.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private String error;
+
+    @JsonProperty("error_description")
+    private String errorDescription;
+}
