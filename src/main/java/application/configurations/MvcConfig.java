d29a7dd23c36025e5396e4c3f0574e2f31340bf6
@khanzhin_aleksandr <bebezaz@yandex.ru>
2021-07-30 12:51:34 +0500

    added MvcConfig2

--- src/main/java/application/configurations/MvcConfig.java
+++ src/main/java/application/configurations/MvcConfig.java
@@ -2,8 +2,9 @@ package application.configurations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@EnableWebMvc
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
