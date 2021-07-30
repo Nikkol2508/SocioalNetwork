c41fb8d623176122a06cddb9165780095190f937
@khanzhin_aleksandr <bebezaz@yandex.ru>
2021-07-30 18:34:18 +0500

    change MvcConfig

--- src/main/java/application/configurations/MvcConfig.java
+++ src/main/java/application/configurations/MvcConfig.java
@@ -1,15 +1,37 @@
 package application.configurations;
 
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-@EnableWebMvc
-@Configuration
-public class MvcConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowCredentials(true).allowedMethods("*");
+//@EnableWebMvc
+@Configuration
+//@ComponentScan(basePackages = { "application.controllers" })
+public class MvcConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowCredentials(true)
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
     }
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**").allowedOrigins("*").allowCredentials(true).allowedMethods("*");
+//    }
+
+//    @Override
+//    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/**").addResourceLocations("/resources/**");
+//    }
+
+
 }
