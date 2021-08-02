49ea703a6680bdaba2adaaf87e8ea0cd1a6f369a
Vitaly Pokrova <vitalik_1254@mail.ru>
2021-08-02 20:18:03 +0300

    created PostMapper, PostDto and User classes, little fixes

--- src/main/java/application/models/Post.java
+++ src/main/java/application/models/Post.java
@@ -1,25 +1,13 @@
 package application.models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-
-import java.util.List;
 
 @Data
 public class Post {
     private int id;
     private long time;
-    private PersonDto author;
+    private int authorId;
     private String title;
-    private int likes;
-    private List<Comment> comments;
-
-    @JsonProperty("post_text")
     private String postText;
-
-    @JsonProperty("is_blocked")
     private boolean isBlocked;
-
-
 }
-
