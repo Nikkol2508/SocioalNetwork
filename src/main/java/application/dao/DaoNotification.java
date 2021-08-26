158f14ef8da4355b257bfc2457966fb007dfafa2
Andrei PC <falocrin@gmail.com>
2021-08-26 16:09:35 +0300

    litle fixes

--- src/main/java/application/dao/DaoNotification.java
+++ src/main/java/application/dao/DaoNotification.java
@@ -25,15 +25,6 @@ public class DaoNotification {
         return jdbcTemplate.query(selectNotifications, new Object[]{id} ,new NotificationMapper());
     }
 
-    public void addNotificationForFriendRequest(int id, long sentTime, int entityId, String contact) {
-        String insertIntoNotificationsStatus = "INSERT INTO notification_type (code, name) VALUES (?, ?)";
-        jdbcTemplate.update(insertIntoNotificationsStatus, NotificationType.FRIEND_REQUEST.toString(),
-                "Заявка на добавление в друзья");
-        String insertIntoNotifications = "INSERT INTO notification (type_id, send_time, person_id, entity_id, contact)" +
-                " VALUES ((SELECT max(notification_type.id) FROM notification_type), ?, ?, ?, ?)";
-        jdbcTemplate.update(insertIntoNotifications, sentTime, id, entityId, contact);
-    }
-
     public List<NotificationsSettingsDto> getNotificationsSettings (int id) {
         String select = "SELECT code, status FROM notification_setting_type JOIN notification_settings ns " +
                 "on notification_setting_type.id = ns.type_id WHERE person_id = ?";
@@ -46,5 +37,15 @@ public class DaoNotification {
         String insertNotificationsType = "INSERT INTO notification_settings (person_id, type_id) VALUES (?," +
                 " (SELECT max(notification_setting_type.id) FROM notification_setting_type))";
         jdbcTemplate.update(insertNotificationsType, id);
+    }
+
+    public void addNotification(int id, long sentTime, int entityId, String contact,
+                                String type, String... name) {
+        String insertIntoNotificationsStatus = "INSERT INTO notification_type (code, name) VALUES (?, ?)";
+        jdbcTemplate.update(insertIntoNotificationsStatus, type,
+                name[0]);
+        String insertIntoNotifications = "INSERT INTO notification (type_id, send_time, person_id, entity_id, contact)" +
+                " VALUES ((SELECT max(notification_type.id) FROM notification_type), ?, ?, ?, ?)";
+        jdbcTemplate.update(insertIntoNotifications, sentTime, id, entityId, contact);
     }
 }
