50e7ed13ebabd4b3d0de6b0a96839127bf8b433c
Vitaly Pokrova <vitalik_1254@mail.ru>
2021-09-06 20:09:43 +0300

    file description fixed

--- src/main/java/application/models/FileDescription.java
+++ src/main/java/application/models/FileDescription.java
@@ -5,13 +5,13 @@ import lombok.Data;
 @Data
 public class FileDescription {
 
-  private int id;
-  private int ownerId;
-  private String name;
-  private String relativeFilePath;
-  private String rawFileURL;
-  private String format;
-  private int bytes;
-  private String type;
-  private long time;
+    private int id;
+    private int ownerId;
+    private String fileName;
+    private String relativeFilePath;
+    private String rawFileURL;
+    private String fileFormat;
+    private int bytes;
+    private String fileType;
+    private long createdAt;
 }
