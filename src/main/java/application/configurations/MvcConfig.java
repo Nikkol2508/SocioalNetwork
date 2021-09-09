34f5f9e02119aad28ae286fc29f12c655259d995
Grommash88 <lakeev.nikolay2016@yandex.ru>
2021-09-09 21:15:49 +0300

    Swagger add

--- src/main/java/application/configurations/MvcConfig.java
+++ src/main/java/application/configurations/MvcConfig.java
@@ -1,18 +1,22 @@
 package application.configurations;
 
+import javax.servlet.MultipartConfigElement;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.unit.DataSize;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.servlet.MultipartConfigElement;
 
 @EnableAutoConfiguration
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+
+  @Value("${prop.swagger.enabled:false}")
+  private boolean enableSwagger;
 
   @Bean
   MultipartConfigElement multipartConfigElement() {
@@ -24,6 +28,19 @@ public class MvcConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**").allowedOrigins("*").allowCredentials(true).allowedMethods("*");
+    registry.addMapping("/**")
+        .allowedOrigins("*")
+        .allowCredentials(true)
+        .allowedMethods("*");
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    if (enableSwagger) {
+      registry.addResourceHandler("swagger-ui.html")
+          .addResourceLocations("classpath:/META-INF/resources/");
+      registry.addResourceHandler("/webjars/**")
+          .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
   }
 }
--- src/main/java/application/configurations/SecurityConfig.java
+++ src/main/java/application/configurations/SecurityConfig.java
@@ -20,41 +20,59 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final JwtTokenProvider jwtTokenProvider;
 
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
+  private final JwtTokenProvider jwtTokenProvider;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .httpBasic().disable()
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
-                    .antMatchers("/*", "/static/**", "/api/v1/auth/*", "/api/v1/platform/*", "/api/v1/account/register").permitAll()
-                    .antMatchers("/api/v1/account/password/*", "/api/v1/account/email").permitAll()
-                    .antMatchers("/storage/*").permitAll()
-                    .anyRequest().hasAuthority(Permission.USER.getPermission())
-                .and()
-                .exceptionHandling()
-                    .accessDeniedHandler(accessDeniedHandler()).authenticationEntryPoint(authenticationEntryPoint())
-                .and()
-                .apply(new JwtConfigurer(jwtTokenProvider));
-    }
+  private static final String[] AUTH_WHITELIST = {
+      "/v2/api-docs",
+      "/swagger-resources",
+      "/swagger-resources/**",
+      "/configuration/ui",
+      "/configuration/security",
+      "/swagger-ui.html",
+      "/webjars/**",
+      "/*",
+      "/static/**",
+      "/api/v1/auth/*",
+      "/api/v1/platform/*",
+      "/api/v1/account/register",
+      "/api/v1/account/password/*",
+      "/api/v1/account/email"
+      // other public endpoints of your API may be appended to this array
+  };
+
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .httpBasic().disable()
+        .csrf().disable()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .authorizeRequests()
+        .antMatchers(AUTH_WHITELIST).permitAll()
+        .anyRequest().hasAuthority(Permission.USER.getPermission())
+        .and()
+        .exceptionHandling()
+        .accessDeniedHandler(accessDeniedHandler())
+        .authenticationEntryPoint(authenticationEntryPoint())
+        .and()
+        .apply(new JwtConfigurer(jwtTokenProvider));
+  }
 
 
-    @Bean
-    public AccessDeniedHandler accessDeniedHandler() {
-        return new CustomAccessDeniedHandler();
-    }
+  @Bean
+  public AccessDeniedHandler accessDeniedHandler() {
+    return new CustomAccessDeniedHandler();
+  }
 
-    @Bean
-    public AuthenticationEntryPoint authenticationEntryPoint() {
-        return new RestAuthenticationEntryPoint();
-    }
+  @Bean
+  public AuthenticationEntryPoint authenticationEntryPoint() {
+    return new RestAuthenticationEntryPoint();
+  }
 }
