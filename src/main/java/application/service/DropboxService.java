fdaa1afb20f091945f4db326cb503fcf1937ae87
nikolajaksenov <aksenovnikk@yandex.ru>
2021-10-01 10:09:14 +0300

    logs dropbox save corrections

--- src/main/java/application/service/DropboxService.java
+++ src/main/java/application/service/DropboxService.java
@@ -11,14 +11,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.time.LocalDate;
+import java.util.Calendar;
 
 @Slf4j
 public class DropboxService {
 
     private static String ACCESS_TOKEN = "j45ajYtbzMUAAAAAAAAAARzdRDRVoUgtUPodzflFti0_KGFyh6xvANTFallXvuOa";
-    public static int day = LocalDate.now().getDayOfMonth();
-    public static int daysBeforeDeletion = 1;
+    public static Calendar calendar = Calendar.getInstance();
+    public static int daysBeforeDeletion = -5;
 
     private static DbxClientV2 getDbxClient() {
         DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/java-tutorial").build();
@@ -30,7 +30,7 @@ public class DropboxService {
         File dir = new File("logs/toDropbox");
         for (File file : dir.listFiles()) {
             try (InputStream in = new FileInputStream("logs/toDropbox/" + file.getName())) {
-                getDbxClient().files().uploadBuilder("/logs/" + day + "/" + file.getName())
+                getDbxClient().files().uploadBuilder("/logs/" + calendar.get(Calendar.DAY_OF_MONTH) + "/" + file.getName())
                         .uploadAndFinish(in);
                 Files.delete(Paths.get("logs/toDropbox/" + file.getName()));
             }
@@ -39,8 +39,9 @@ public class DropboxService {
 
     public static void deleteFromDropbox() {
 
+        calendar.roll(Calendar.DAY_OF_MONTH, daysBeforeDeletion);
         try {
-            getDbxClient().files().deleteV2("/logs/" + (day - daysBeforeDeletion));
+            getDbxClient().files().deleteV2("/logs/" + calendar.get(Calendar.DAY_OF_MONTH));
         } catch (DbxException e) {
             log.error("File not found: stecTrace {}", e.getStackTrace());
         }
